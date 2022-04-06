016a215be966791550b7a4d55bc96608f4ffb8e6
gilberto <gsqwow@gmail.com>
2022-04-06 10:08:19 -0600

    Add clear button functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid');
+const clear = document.querySelector('#clear');
 
 for (let i = 0; i < 256; i++){
     const gridDiv = document.createElement('div');
@@ -8,5 +9,8 @@ for (let i = 0; i < 256; i++){
     gridDiv.addEventListener('mouseenter', () => {
         gridDiv.style.backgroundColor = 'black';
     });
-};
 
+    clear.addEventListener('click', () => {
+        gridDiv.style.backgroundColor = 'white';
+    })
+};
