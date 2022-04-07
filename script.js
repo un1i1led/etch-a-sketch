5a75b969709d82498e22cf1be1757c6292f42f9d
gilberto <gsqwow@gmail.com>
2022-04-06 20:25:00 -0600

    Add size functionality

--- script.js
+++ script.js
@@ -1,16 +1,87 @@
+const container = document.querySelector('.container');
 const grid = document.querySelector('.grid');
 const clear = document.querySelector('#clear');
+const small = document.querySelector('#small');
+const medium = document.querySelector('#medium');
+const large = document.querySelector('#large');
 
-for (let i = 0; i < 256; i++){
-    const gridDiv = document.createElement('div');
-    gridDiv.className = 'gridDiv';
-    grid.appendChild(gridDiv);
+let smallChoice = () => {
+    for (let i = 0; i < 256; i++){
+        const gridDiv = document.createElement('div');
+        gridDiv.id = i;
+        gridDiv.className = 'gridDiv';
+        grid.appendChild(gridDiv);
+        
+    
+        gridDiv.addEventListener('mouseenter', () => {
+            gridDiv.style.backgroundColor = 'black';
+        });
+    
+        clear.addEventListener('click', () => {
+            gridDiv.style.backgroundColor = 'white';
+        });
 
-    gridDiv.addEventListener('mouseenter', () => {
-        gridDiv.style.backgroundColor = 'black';
-    });
+        small.addEventListener('click', () => {
+            gridDiv.style.backgroundColor = 'white';
+        })
 
-    clear.addEventListener('click', () => {
-        gridDiv.style.backgroundColor = 'white';
-    })
+        medium.addEventListener('click', () => {
+            gridDiv.style.backgroundColor = 'white';
+        });
+
+        
+    };    
 };
+
+let mediumChoice = () => {
+    for (let i = 256; i < 1024; i++){
+        const gridDiv = document.createElement('div');
+        gridDiv.id = i;
+        gridDiv.className = 'gridDiv';
+        grid.appendChild(gridDiv);
+
+        gridDiv.addEventListener('mouseenter', () => {
+            gridDiv.style.backgroundColor = 'black';
+        });
+
+        clear.addEventListener('click', () => {
+            gridDiv.style.backgroundColor = 'white';
+        });
+
+        small.addEventListener('click', () => {
+            gridDiv.remove();
+        });
+        
+    };
+};
+
+let largeChoice = () => {
+    for (let i = 1024; i < 4096; i++){
+        const gridDiv = document.createElement('div');
+        gridDiv.id = i;
+        gridDiv.className = 'gridDiv';
+        grid.appendChild(gridDiv);
+
+        gridDiv.addEventListener('mouseenter', () => gridDiv.style.backgroundColor = 'black');
+
+        clear.addEventListener('click', () => {
+            gridDiv.style.backgroundColor = 'white';
+        });
+
+        small.addEventListener('click', () => {
+            gridDiv.remove();
+        });
+
+        medium.addEventListener('click', () => {
+            gridDiv.remove();
+        });
+    }
+};
+
+
+
+smallChoice();
+
+medium.addEventListener('click', mediumChoice);
+large.addEventListener('click', largeChoice);
+
